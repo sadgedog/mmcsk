863c30e9727e27d16fa44142c9dbeeed3968602b
sadgedog <sorachi1231.214@gmail.com>
2022-05-30 19:55:36 +0900

    fix

--- zero_octal.c
+++ zero_octal.c
@@ -3,11 +3,3 @@
 #include <ctype.h>
 
 // https://stackoverflow.com/questions/6895522/is-0-a-decimal-literal-or-an-octal-literal
-
-int main() {
-  int foo[3] = {0, 1, 2};
-  printf("%p\n", foo);
-  printf("%p\n", foo+1);
-  printf("%p\n", foo+2);
-  
-}
